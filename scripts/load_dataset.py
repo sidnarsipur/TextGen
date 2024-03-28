bec266fd746bd7a0d8fe521e331f3979919a80eb
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2024-03-28 19:01:19 -0400

    Update load_dataset.py

--- scripts/load_dataset.py
+++ scripts/load_dataset.py
@@ -1,40 +1,46 @@
 from datasets import load_dataset
-
-from PIL import Image
-
 import os
 
-os.makedirs("data/train", exist_ok=True)
-
+#Load HuggingFace Dataset
 ds = load_dataset(
         "gvecchio/MatSynth",
         split = "train",
         streaming=True
         )
 
+#Create train directory
+os.makedirs("data/train", exist_ok=True)
+
+#Loop over dataset
 for idx, d in enumerate(ds):
     name = str(idx)
     path = "data/train"
 
-    os.mkdir(path + "/" + name)
+    #Create data directory
+    os.mkdir(os.path.join(path, name))
 
+    #Load Maps
     basecolor = d['basecolor']
     height = d['height']
     normal = d['normal']
     roughness = d['roughness']
 
+    #Resize to 1024x1024
     basecolor_scaled = basecolor.resize((1024, 1024))
     height_scaled = height.resize((1024, 1024))
     normal_scaled = normal.resize((1024, 1024))
     roughness_scaled = roughness.resize((1024, 1024))
 
+    #Save resized maps locally as PNG
     basecolor_scaled.save(os.path.join(path, name, "basecolor.png"), "PNG")
     height_scaled.save(os.path.join(path, name, "height.png"), "PNG")
     normal_scaled.save(os.path.join(path, name, "normal.png"), "PNG")
     roughness_scaled.save(os.path.join(path, name, "roughness.png"), "PNG")
 
+    #Save metadata locally
     with open(os.path.join(path, name, "desc.txt"), 'w') as file:
         file.write(f"Tags: {', '.join(d['metadata']['tags'])}\nName: {d['metadata']['name']}")
 
+    #Confirmation Print
     print("Image No. {} -> {} saved".format(idx, d['metadata']['name']))
 
