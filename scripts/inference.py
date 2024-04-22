d1f464ac9964a5acf03f45e07e2e657a7c65d862
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2024-04-22 02:20:02 -0400

    Inference Script

---
+++ scripts/inference.py
@@ -0,0 +1,28 @@
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
+from diffusers.utils import load_image
+import torch
+
+base_model_path = "stabilityai/stable-diffusion-2-1-base"
+controlnet_path = "sidnarsipur/controlnet_normal"
+
+controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch.float16)
+pipe = StableDiffusionControlNetPipeline.from_pretrained(
+    base_model_path, controlnet=controlnet, torch_dtype=torch.float16
+)
+
+# speed up diffusion process with faster scheduler and memory optimization
+pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
+# remove following line if xformers is not installed or when using Torch 2.0.
+pipe.enable_xformers_memory_efficient_attention()
+# memory optimization.
+pipe.enable_model_cpu_offload()
+
+control_image = load_image("inference/basecolor.png")
+prompt = "Normal Map"
+
+# generate image
+generator = torch.manual_seed(0)
+image = pipe(
+    prompt, num_inference_steps=20, generator=generator, image=control_image
+).images[0]
+image.save("inference/normal.png")
---
+++ scripts/train.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=Normal
+#SBATCH --error=logs/error.txt
+#SBATCH --output=logs/error.txt
+#SBATCH --time=0-8:00
+#SBATCH -c 2 --mem=132G
+#SBATCH -p gpu --gres=gpu:1 -C A100
+
+module load miniconda3
+eval "$(conda shell.bash hook)"
+conda activate rough
+  
+accelerate launch diffusers/examples/controlnet/train_controlnet.py \
+--pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
+--controlnet_model_name_or_path="sidnarsipur/controlnet_normal" \
+--output_dir="controlnet_normal" \
+--cache_dir=base_models \
+--dataset_name="base_models/sidnarsipur___controlnet_data" \
+--conditioning_image_column=basecolor \
+--image_column=normal \
+--resolution=512 \
+--learning_rate=1e-4 \
+--proportion_empty_prompts=1.0 \
+--empty_prompt_sub="Normal Map" \
+--validation_image "validation/basecolor_88.png" "validation/basecolor_128.png" "validation/basecolor_176.png" "validation/basecolor_37543.png" "validation/basecolor_38773.png" \
+--validation_prompt "Normal Map" "Normal Map" "Normal Map" "Normal Map" "Normal Map" \
+--train_batch_size=12 \
+--num_train_epochs=3 \
+--tracker_project_name="controlnet_normal" \
+--enable_xformers_memory_efficient_attention \
+--checkpointing_steps=2600 \
+--report_to wandb \
+--validation_steps=1300 \
+--logging_dir="logs" \
+--push_to_hub
+
+conda deactivate
+exit 0
